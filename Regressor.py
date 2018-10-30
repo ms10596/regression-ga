5f2d2e8dcb22e976ec141e6e0b87a991e1163a73
Mohamed Sayed <ms10596@gmail.com>
2018-10-30 14:31:16 +0200

    feat: add polynomial degrees to x

--- Regressor.py
+++ Regressor.py
@@ -1,20 +1,24 @@
-
+import numpy
 
 class Regressor:
     def __init__(self, args):
         self.points_n = args[0]
         self.degree = args[1]
-        self.x = [args[2]]
-        self.y = args[3]
-        # print(self.x)
-        # print(self.x.shape[0])
-        # print(self.x.shape[1])
+        self.x = numpy.array([args[2]])
+        self.y = numpy.array(args[3])
+
         self.add_degrees()
-        print(len(self.x))
+        print(self.x[0])
+        print(self.x[1])
+        print(self.x[2])
 
     def add_degrees(self):
+        """add polynomial degrees to the x"""
         for i in range(2, self.degree+1):
-            self.x.append([pow(j, i) for j in self.x[0]])
+            self.x = numpy.concatenate((self.x, numpy.array([[pow(j, i) for j in self.x[0]]])), axis=0)
+        self.x = numpy.concatenate(([numpy.ones(64,)], self.x), axis=0)
+
+
 
 
 
