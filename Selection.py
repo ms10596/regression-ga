60774e7bac8746270b4121ba682c2676db6f04d6
Mohamed Sayed <ms10596@gmail.com>
2018-11-02 14:54:40 +0200

    feat: select pairs and n_pairs

--- Selection.py
+++ Selection.py
@@ -3,6 +3,14 @@ import numpy
 import random
 
 
+def select_n_pairs(n, population, x, y):
+    return [select_pair(population, x, y) for i in range(n)]
+
+
+def select_pair(population, x, y):
+    return select(population, x, y), select(population, x, y)
+
+
 def select(population, x, y):
     """since we prefer chromosomes with small mse, Our roulette wheel will
     behave different. We will start with all fitness of all population, then
@@ -36,4 +44,4 @@ if __name__ == '__main__':
     x = numpy.array([[1, 1, 1, 1, 1], [1, 2, 3, 4, 5], [1, 4, 9, 16, 25]])
     y = numpy.array([1, 4, 9, 16, 25])
 
-    print(select(population, x, y))
+    print(select_n_pairs(2, population, x, y))
