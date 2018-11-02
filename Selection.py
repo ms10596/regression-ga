431e657ded4acb9f4815fb153d1eca3d0e041b23
Mohamed Sayed <ms10596@gmail.com>
2018-11-02 14:49:21 +0200

    feat: selection with inverse roulette wheel

--- Selection.py
+++ Selection.py
@@ -1,9 +1,24 @@
 from numpy import sum, power, dot
 import numpy
+import random
 
 
-def select():
-    return ""
+def select(population, x, y):
+    """since we prefer chromosomes with small mse, Our roulette wheel will
+    behave different. We will start with all fitness of all population, then
+    we start removing fitness of every chromosome. if fitness is small a bigger
+    chance to be choosen and vice versa"""
+    all_fitness = get_all_fitness(population, x, y)
+    # print(all_fitness)
+    lucky = random.uniform(0, all_fitness)
+    # print("lucky:", lucky)
+    for chromosome in population:
+        # print("fitness of chromosome", fitness(x, y, chromosome))
+        all_fitness -= fitness(x, y, chromosome)
+        # print("all fitness:", all_fitness)
+        if lucky <= all_fitness:
+            return chromosome
+    return population[len(population) - 1]
 
 
 def fitness(x, y, theta):
@@ -11,11 +26,14 @@ def fitness(x, y, theta):
     return sum(power((dot(x.transpose(), theta) - y), 2)) / len(x)
 
 
-def get_all_fitness(population):
-    return sum([fitness(i) for i in population])
+def get_all_fitness(population, x, y):
+    return sum([fitness(x, y, i) for i in population])
+
 
 if __name__ == '__main__':
-    thetas = numpy.array([0, 0, 1])
+    thetas = numpy.array([0, 0, 0])
+    population = [thetas, numpy.array([0, 0, 1]), numpy.array([1, 0, 0]), numpy.array([0, 1, 0])]
     x = numpy.array([[1, 1, 1, 1, 1], [1, 2, 3, 4, 5], [1, 4, 9, 16, 25]])
     y = numpy.array([1, 4, 9, 16, 25])
-    print(fitness(x, y, thetas))
+
+    print(select(population, x, y))
